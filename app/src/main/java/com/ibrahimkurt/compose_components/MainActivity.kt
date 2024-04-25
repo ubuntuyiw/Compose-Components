291e189a99a26197299da7b05d54fd76171d73ad
İBRAHİM KURT <ibrahim.kurt@ubuntuyouiwe.com>
2024-04-25 22:55:19 +0300

    init

---
+++ app/src/main/java/com/ibrahimkurt/compose_components/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.ibrahimkurt.compose_components
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ibrahimkurt.compose_components.ui.theme.ComposeComponentsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
